6fbff27a6959d9c0e1dc6634186e18f3d864f295
Arnout Cator <arnout.cator@paconsulting.com>
2022-09-22 10:08:01 +0100

    added comment in main class

--- src/main/java/com/demo/payroll/PayrollApplication.java
+++ src/main/java/com/demo/payroll/PayrollApplication.java
@@ -3,6 +3,9 @@ package com.demo.payroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Demonstration of Spring boot (true HATEOS) Restful Java web service application
+ */
 @SpringBootApplication
 public class PayrollApplication {
 
